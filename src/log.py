35868b3f7ca67f68a4e8f5c45e771be6243e4448
RaghuRam2005 <atommail20@gmail.com>
2025-03-16 10:20:39 +0530

    update all

---
+++ src/log.py
@@ -0,0 +1,39 @@
+import csv
+import datetime
+
+class ConnectionLogger:
+    def __init__(self):
+        self.logs = []
+        self.logging_active = False
+        self.is_logging = False
+        self.monitor = None  # Will be set from outside
+        self.file_name = ".\\logs\\connection_logs.csv"
+
+    def set_monitor(self, monitor):
+        """Set the monitor reference without creating a circular dependency"""
+        self.monitor = monitor
+
+    def start_logging(self):
+        self.logging_active = True
+        self.is_logging = True
+        print(f"Started logging from {datetime.datetime.now()}")
+        if self.monitor:
+            self.monitor.start_monitoring()
+
+    def stop_logging(self):
+        if self.logging_active:
+            self.logging_active = False
+            self.is_logging = False
+        if self.monitor:
+            self.monitor.stop_monitoring()
+
+    def log_connection(self, src_ip, dst_ip, dst_port, action):
+        try:
+            if not hasattr(self, 'file_name') or not self.file_name:
+                self.file_name = "connection_logs.csv"
+                
+            with open(self.file_name, 'a', newline='') as csvfile:
+                writer = csv.writer(csvfile, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                writer.writerow([datetime.datetime.now().isoformat(), src_ip, dst_ip, dst_port, action])
+        except Exception as e:
+            print(f"Exception occurred while logging connection. {e}")
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,6 @@
 import ctypes
 import platform
 import os
-import json
 
 def check_admin_privileges():
     if platform.system() == "Windows":
@@ -29,33 +28,6 @@ def show_blocked(config):
     print("Blocked IPs: ")
     for ip in config["blocked_ips"]:
         print(f"{ip}")
-    print("Blocked sites")
+    print("Blocked sites: ")
     for site in config["blocked_sites"]:
         print(f"{site}")
-
-def load_config(config, config_file):
-    if os.path.exists(config_file):
-        try:
-            with open(config_file, 'r') as f:
-                loaded_config = json.load(f)
-                config.update(loaded_config)
-            print(f"Configuretion loaded from {config_file}")
-            return config
-        except Exception as e:
-            print(f"Error while loading configuration: {e}")
-            return config
-    else:
-        print(f"No configuration file found at {config_file}")
-        return save_config(config, config_file)
-
-def save_config(config, config_file):
-    try:
-        with open(config_file, 'w') as f:
-            json.dump(config, f, indent=4)
-        print(f"Configuration saved to {config_file}")
-        return config
-    except Exception as e:
-        print(f"Error saving configuration: {e}")
-        return config
-    
-
